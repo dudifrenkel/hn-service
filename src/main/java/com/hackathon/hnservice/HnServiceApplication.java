c5fb0a461d23dcf5bcf936671889d68ffcea3530
maorbe <maorbe@amdocs.com>
2022-12-20 14:44:53 +0200

    add prometheus
    config actuator

--- src/main/java/com/hackathon/hnservice/HnServiceApplication.java
+++ src/main/java/com/hackathon/hnservice/HnServiceApplication.java
@@ -2,8 +2,10 @@ package com.hackathon.hnservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.hackathon")
 public class HnServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hackathon/hnservice/SleepyRestClient.java
+++ src/main/java/com/hackathon/hnservice/SleepyRestClient.java
@@ -1,14 +1,20 @@
 package com.hackathon.hnservice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class SleepyRestClient {
-    private String url = "http://localhost:8081/sleep";
-    private RestTemplate restTemplate = new RestTemplate();
+    private final String url = "http://localhost:8081/sleep";
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    public String callSleepy(){
-        return restTemplate.getForObject(url, String.class);
+    public String callSleepy() {
+        try{
+            return restTemplate.getForObject(url, String.class);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "sleepy is not available", e);
+        }
     }
 }
