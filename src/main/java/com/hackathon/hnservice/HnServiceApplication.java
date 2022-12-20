f27c669b85e57bb784d439b678cfecef05a045ed
dudif <dudif@amdocs.com>
2022-12-20 20:12:47 +0200

    virtualthreads fix path add another api - no external

--- src/main/java/com/hackathon/hnservice/HnServiceApplication.java
+++ src/main/java/com/hackathon/hnservice/HnServiceApplication.java
@@ -2,7 +2,13 @@ package com.hackathon.hnservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class HnServiceApplication {
@@ -11,4 +17,18 @@ public class HnServiceApplication {
 		SpringApplication.run(HnServiceApplication.class, args);
 	}
 
+	@Bean
+	AsyncTaskExecutor applicationTaskExecutor() {
+		// enable async servlet support
+		ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+		return new TaskExecutorAdapter(executorService::execute);
+	}
+
+	@Bean
+	TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+
+		return protocolHandler -> {
+			protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+		};
+	}
 }
