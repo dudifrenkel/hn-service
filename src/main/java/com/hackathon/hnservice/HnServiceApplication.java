0773005957b343266836c9791bfb986f58ab89b4
maorbe <maorbe@amdocs.com>
2022-12-20 14:52:22 +0200

    remove component scan

--- src/main/java/com/hackathon/hnservice/HnServiceApplication.java
+++ src/main/java/com/hackathon/hnservice/HnServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.hackathon")
 public class HnServiceApplication {
 
 	public static void main(String[] args) {
