9ea314fa6422bcdb861faf9e71b2ff377cfeb844
dudif <dudif@amdocs.com>
2022-12-29 17:13:03 +0200

    file, logger

--- src/main/java/com/hackathon/hnservice/Controller.java
+++ src/main/java/com/hackathon/hnservice/Controller.java
@@ -1,5 +1,8 @@
 package com.hackathon.hnservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,9 @@ public class Controller {
     final long DEFAULT_SLEEP = 100;
     private final SleepyRestClient sleepyRestClient;
 
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     public Controller(SleepyRestClient sleepyRestClient) {
         this.sleepyRestClient = sleepyRestClient;
     }
@@ -28,25 +34,25 @@ public class Controller {
         iterations = iterations == null ? DEFAULT_ITERATIONS : iterations;
         sleep = sleep == null ? DEFAULT_SLEEP : sleep;
 
-        System.out.printf("New request: %s, %d%n", Thread.currentThread(), Thread.currentThread().getId());
+        logger.debug(String.format("New request: %s, %d%n", Thread.currentThread(), Thread.currentThread().getId()));
         try {
-            InputStream is = Files.newInputStream(Paths.get("/Users/dudif/IdeaProjects/hn-service/src/main/resources/file.json"));
+            InputStream inputStream = new ClassPathResource("file.json").getInputStream();
             MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
 
+            logger.debug("start processing");
             for (int i = 0; i < iterations; i++) {
-                sha256.digest(is.readAllBytes());
+                sha256.digest(inputStream.readAllBytes());
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
+            logger.debug("end processing");
+        } catch (IOException | NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.printf("start sleep for: %d%n", sleep);
+        logger.debug(String.format("start sleep for: %d%n", sleep));
         Thread.sleep(sleep);
-        System.out.printf("end sleep");
+        logger.debug("end sleep");
 
-        System.out.printf("End request: %d\n\n", Thread.currentThread().getId());
+        logger.debug(String.format("End request: %d\n\n", Thread.currentThread().getId()));
         return String.format("Iterations number: %d - done with thread.id %d", iterations, Thread.currentThread().getId());
     }
 
@@ -75,5 +81,4 @@ public class Controller {
         System.out.printf("End request: %d\n\n", Thread.currentThread().getId());
         return String.format("Iterations number: %d - done with thread.id %d", iterations, Thread.currentThread().getId());
     }
-
 }
