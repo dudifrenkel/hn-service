4c67d702b09c513108a6e834dfc38102964dae73
dudif <dudif@amdocs.com>
2023-01-02 17:31:01 +0200

    file, logger

--- src/main/java/com/hackathon/hnservice/Controller.java
+++ src/main/java/com/hackathon/hnservice/Controller.java
@@ -1,5 +1,7 @@
 package com.hackathon.hnservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,9 @@ public class Controller {
     final long DEFAULT_SLEEP = 100;
     private final SleepyRestClient sleepyRestClient;
 
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     public Controller(SleepyRestClient sleepyRestClient) {
         this.sleepyRestClient = sleepyRestClient;
     }
@@ -28,6 +33,32 @@ public class Controller {
         iterations = iterations == null ? DEFAULT_ITERATIONS : iterations;
         sleep = sleep == null ? DEFAULT_SLEEP : sleep;
 
+        logger.debug(String.format("New request: %s, %d%n", Thread.currentThread(), Thread.currentThread().threadId()));
+        try {
+            InputStream in = this.getClass().getResourceAsStream("/file.json");
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+
+            logger.debug("start processing");
+            for (int i = 0; i < iterations; i++) {
+                sha256.digest(in.readAllBytes());
+            }
+            logger.debug("end processing");
+        } catch (IOException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.debug(String.format("start sleep for: %d%n", sleep));
+        Thread.sleep(sleep);
+        logger.debug("end sleep");
+
+        logger.debug(String.format("End request: %d\n\n", Thread.currentThread().threadId()));
+        return String.format("Iterations number: %d - done with thread.id %d", iterations, Thread.currentThread().threadId());
+    }
+
+    @GetMapping("/test")
+    public String test(@RequestParam(required = false) Long iterations) {
+        iterations = iterations == null ? DEFAULT_ITERATIONS : iterations;
+
         System.out.printf("New request: %s, %d%n", Thread.currentThread(), Thread.currentThread().threadId());
         try {
             InputStream is = Files.newInputStream(Paths.get("/Users/dudif/IdeaProjects/hn-service/src/main/resources/file.json"));
@@ -36,35 +67,7 @@ public class Controller {
             for (int i = 0; i < iterations; i++) {
                 sha256.digest(is.readAllBytes());
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.printf("start sleep for: %d%n", sleep);
-        Thread.sleep(sleep);
-        System.out.printf("end sleep");
-
-        System.out.printf("End request: %d\n\n", Thread.currentThread().threadId());
-        return String.format("Iterations number: %d - done with thread.id %d", iterations, Thread.currentThread().getId());
-    }
-
-    @GetMapping("/test")
-    public String test(@RequestParam(required = false) Long iterations) {
-        iterations = iterations == null ? DEFAULT_ITERATIONS : iterations;
-
-        System.out.printf("New request: %d%n", Thread.currentThread().threadId());
-        try {
-            InputStream is = Files.newInputStream(Paths.get("src/main/resources/file.json"));
-            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-
-            for (int i = 0; i < iterations; i++) {
-                sha256.digest(is.readAllBytes());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
+        } catch (IOException | NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
 
@@ -73,7 +76,6 @@ public class Controller {
         System.out.printf("end call sleepy, res:%s%n", res);
 
         System.out.printf("End request: %d\n\n", Thread.currentThread().threadId());
-        return String.format("Iterations number: %d - done with thread.id %d", iterations, Thread.currentThread().getId());
+        return String.format("Iterations number: %d - done with thread.id %d", iterations, Thread.currentThread().threadId());
     }
-
 }
